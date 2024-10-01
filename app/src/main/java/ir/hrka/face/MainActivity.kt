fc9ed2841520f4def94e5856af106d0c7ada3010
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-10-01 17:24:34 +0330

    Init commit

---
+++ app/src/main/java/ir/hrka/face/MainActivity.kt
@@ -0,0 +1,15 @@
+package ir.hrka.face
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {}
+    }
+}
