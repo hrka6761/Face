7ed9b39ed7aabc17b0542f25a23d2921ea41b126
HAMIDREZA <hrka6761gmail.com>
2024-10-07 11:02:18 +0330

    Changing SplashScreenPreview function parameters to resolve preview

--- app/src/main/java/ir/hrka/face/presentation/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/ir/hrka/face/presentation/ui/screens/splash/SplashScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -107,5 +106,5 @@ fun SplashScreen(activity: MainActivity, navHostController: NavHostController) {
 @Preview(showBackground = true)
 @Composable
 fun SplashScreenPreview() {
-    SplashScreen(LocalContext.current as MainActivity, rememberNavController())
+    SplashScreen(MainActivity(), rememberNavController())
 }
