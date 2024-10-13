4719589fcbc81eab16eee0e4a25f3fdd66aa217a
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-10-13 15:53:27 +0330

    Implementing base of face detection and display number of detected faces on logcat

--- app/src/main/java/ir/hrka/face/di/GlobalModule.kt
+++ app/src/main/java/ir/hrka/face/di/GlobalModule.kt
@@ -1,8 +1,12 @@
 package ir.hrka.face.di
 
 import android.content.Context
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetector
+import com.google.mlkit.vision.face.FaceDetectorOptions
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,8 +23,28 @@ class GlobalModule {
     fun provideCameraProvider(@ApplicationContext context: Context): ProcessCameraProvider =
         ProcessCameraProvider.getInstance(context).get()
 
+    @Provides
+    @Singleton
+    fun providePreview(): Preview = Preview.Builder().build()
 
     @Provides
     @Singleton
-    fun getPreview(): Preview = Preview.Builder().build()
+    fun provideImageAnalysis(): ImageAnalysis = ImageAnalysis.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideFaceDetectorOptions(): FaceDetectorOptions =
+        FaceDetectorOptions.Builder()
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
+            .setContourMode(FaceDetectorOptions.CONTOUR_MODE_NONE)
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_NONE)
+            .setMinFaceSize(0.1f)
+            .enableTracking()
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideFaceDetector(options: FaceDetectorOptions): FaceDetector =
+        FaceDetection.getClient(options)
 }
--- app/src/main/java/ir/hrka/face/presentation/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/ir/hrka/face/presentation/ui/screens/home/HomeViewModel.kt
@@ -4,13 +4,20 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.hardware.camera2.CameraMetadata.LENS_FACING_BACK
 import android.util.Log
+import androidx.annotation.OptIn
 import androidx.camera.core.Camera
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetector
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import ir.hrka.face.R
@@ -27,7 +34,9 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     @ApplicationContext private val context: Context,
     private val cameraProvider: ProcessCameraProvider,
-    private val preview: Preview
+    private val preview: Preview,
+    private val imageAnalysis: ImageAnalysis,
+    private val faceDetector: FaceDetector
 ) : ViewModel() {
 
     private lateinit var _previewView: PreviewView
@@ -39,18 +48,38 @@ class HomeViewModel @Inject constructor(
     private var lensFacing = LENS_FACING_BACK
 
 
+    @OptIn(ExperimentalGetImage::class)
     fun bindPreview(previewView: PreviewView, lifecycleOwner: LifecycleOwner) {
         _previewView = previewView
         _lifecycleOwner = lifecycleOwner
         preview.setSurfaceProvider(previewView.surfaceProvider)
         cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
-        camera = cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector!!, preview)
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(context)) { imageProxy: ImageProxy ->
+            val mediaImage = imageProxy.image
+
+            if (mediaImage != null) {
+                val image =
+                    InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+                faceDetector
+                    .process(image)
+                    .addOnSuccessListener { faces ->
+                        Log.i(TAG, "Success: ${faces.size}")
+                        imageProxy.close()
+                    }
+                    .addOnFailureListener { e ->
+                        Log.i(TAG, "Fail: ${e.message}")
+                    }
+            }
+        }
+        camera =
+            cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector!!, preview, imageAnalysis)
         setFlashLightState()
     }
 
     fun unbindPreview() {
         if (camera != null) {
-            cameraProvider.unbind(preview)
+            cameraProvider.unbind(preview, imageAnalysis)
             camera = null
             cameraSelector = null
         }
